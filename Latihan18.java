8f4049154f3c0c0cba5a00a373fe0e6b1ff3162a
lptpmingika <lptpmingika@gmail.com>
2024-09-10 12:15:32 +0700

    Add files via upload

---
+++ Latihan18.java
@@ -0,0 +1,16 @@
+/** 
+ * Latihan18
+ */
+public class Latihan18 {
+
+    public static void main(String[] args) {
+        System.out.println("Data Mahasiswa Jurusan Teknologi Informasi POLINEMA");
+        System.out.println("Nama     :Saya muhammad ali akhyar Wicaksono dipanggil Mingika");
+        System.out.println("NIM      :244107020080");
+        System.out.println("Kelas    :T informatika 1 a");
+        System.out.println("No.absen :18");
+        System.out.println("alamat   :jalan candi mendut");
+        System.out.println("no.hp    :08384750643");
+        System.out.println("hobi     :-");
+    }
+}
